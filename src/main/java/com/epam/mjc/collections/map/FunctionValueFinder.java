72483cd56aea59a0eac20d40a2b04b875cad93d3
Bakhytzhan <mykty-baha@bk.ru>
2022-10-22 15:03:26 +0600

    map1

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+        return functionMap.containsValue(requiredValue);
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> res = new HashMap<>();
+
+        for (Integer i: sourceList){
+            res.put(i, 5*i+2);
+        }
+
+        return res;
     }
+
 }
