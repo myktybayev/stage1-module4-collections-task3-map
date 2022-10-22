df026172965ed6bd33d68601ca4d0882cd984b6d
Bakhytzhan <mykty-baha@bk.ru>
2022-10-22 15:06:11 +0600

    map3

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> res = new HashMap<>();
+        if(sentence.length()>0) {
+            String word = sentence.toLowerCase().replace(",", "").replace(".", "").trim();
+
+            String[] split = word.split(" ");
+            int c = 1;
+            for (String w : split)
+                if (res.containsKey(w)) {
+                    c = res.get(w);
+                    c++;
+                    res.put(w, c);
+                    c = 1;
+                } else {
+                    res.put(w, c);
+                }
+        }
+        return res;
+
     }
 }
